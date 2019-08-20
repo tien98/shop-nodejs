740f9293fdee12ea57095352e67d5f6aedfe793e
Quach Dang Tien <tienquach36@gmail.com>
2019-08-20 21:56:46 +0700

    a

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(bodyParser.json());
 
 app.use('/products', routerProducts);
 app.use('/orders', routerOrders);
-app.use('/user', cors() , routerUsers);
+app.use('/user', routerUsers);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
