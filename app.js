f2c6c7d94760decd5cc051f8326c99537a8e5149
Quach Dang Tien <tienquach36@gmail.com>
2019-08-20 21:40:27 +0700

    a

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(cors({
   origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
-  optionsSuccessStatus: 204
+  optionsSuccessStatus: 204,
+  credentials: true
 }));
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
