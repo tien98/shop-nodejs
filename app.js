42ef4beb656935616f7aa6e9b0d2393ca5091fd8
Quach Dang Tien <tienquach36@gmail.com>
2019-08-20 14:04:40 +0700

    fix3

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ mongoose.connect('mongodb+srv://'+ process.env.MONGO_ATLAS_US +':'+ process.env.
 mongoose.Promise = global.Promise;
 
 const app = express(); 
-app.use(morgan('dev'));
 app.use(cors());
+app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -40,11 +40,6 @@ app.use('/products', routerProducts);
 app.use('/orders', routerOrders);
 app.use('/user', routerUsers);
 
-app.get('/', (req, res) => {
-        return res.status(200).json({
-            message: 'Hello Boss'
-        })    
-})
 app.use((req, res, next) => {
     const error = new Error('Not found');
     error.status = 404;
