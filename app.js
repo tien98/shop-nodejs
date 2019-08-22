d4188011f24bd790a9799c9532a2b5eabc509b98
tien98 <tienquach36@gmail.com>
2019-08-22 18:52:27 +0700

    6

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://activate.navicat.com:3000");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
