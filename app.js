326e4c303099283e0eb77f6c115a6a305c982f55
tien98 <tienquach36@gmail.com>
2019-08-22 15:38:02 +0700

    fixx cors

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cors({
   origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
-  optionsSuccessStatus: 201,
+  optionsSuccessStatus: 200,
   credentials: true
 }));
 app.use(morgan('dev'));
