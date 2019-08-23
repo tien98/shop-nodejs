ea4b930cedead1085e96e04c8d784f2c271f5052
tien98 <tienquach36@gmail.com>
2019-08-23 18:17:50 +0700

    7

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const http = require('http');
 const app = require('./app');
-    // look for PORT environment variable,
-    // else look for CLI argument,
-    // else use hard coded value for port 8080
-const port =  process.env.PORT || process.argv[2] || 5000;
-
+const port =  process.env.PORT || 5000;
 const server = http.createServer(app);
 
 server.listen(port, ()=> {console.log('Server started...')});
